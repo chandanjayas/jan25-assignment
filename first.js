ce6165d460d7c5f327a9c4fae81d4da3e20128b0
chandan jayas <90899405+chandanjayas@users.noreply.github.com>
2022-01-26 12:28:02 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+var a=10;
+if(a>10){
+console.log("a is greatest number of 10");
+}
+else if(a==10){
+console.log("a is equal number of 10");
+}
+else{
+console.log(" a is not equal number of 10");
+}
